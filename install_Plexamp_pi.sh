51f1eb2519b7b09c8457abfd85cb69e98606d4cd
odinb <odin_b@yahoo.com>
2020-12-15 11:09:42 -0600

    Update install_Plexamp_pi.sh

--- install_Plexamp_pi.sh
+++ install_Plexamp_pi.sh
@@ -181,29 +181,6 @@ if [ ! -f /etc/update-motd.d/20-logo ]; then
 cat >> /etc/update-motd.d/20-logo << 'EOF'
 #!/bin/sh
 
- upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
- secs=$((${upSeconds}%60))
- mins=$((${upSeconds}/60%60))
- hours=$((${upSeconds}/3600%24))
- days=$((${upSeconds}/86400))
- UPTIME=`printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs"`
-
- # get the load averages
- read one five fifteen rest < /proc/loadavg
-
- echo "$(tput setaf 2)
-    .~~.   .~~.    `date +"%A, %e %B %Y, %r"`
-   '. \ ' ' / .'   `uname -srmo`$(tput setaf 1)
-    .~ .~~~..~.
-   : .~.'~'.~. :   Uptime.............: ${UPTIME}
-  ~ (   ) (   ) ~  Memory.............: `cat /proc/meminfo | grep MemFree | awk {'print $2'}`kB (Free) / `cat /proc/meminfo | grep MemTotal | awk {'print $2'}`kB (Total)
- ( : '~'.~.'~' : ) Load Averages......: ${one}, ${five}, ${fifteen} (1, 5, 15 min)
-  ~ .~ (   ) ~. ~  Running Processes..: `ps ax | wc -l | tr -d " "`
-   (  : '~' :  )   IP Addresses.......: `ip a | grep glo | awk '{print $2}' | head -1 | cut -f1 -d/` and `wget -q -O - http://icanhazip.com/ | tail`
-    '~ .~~~. ~'    Weather............: `curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=1&locCode=EUR|UK|UK001|NAILSEA|" | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2°\3, \1/p'`
-        '~'
- $(tput sgr0)"
-
 echo    ""
 echo    ""
 echo    "   ██████╗ ██╗     ███████╗██╗  ██╗ █████╗ ███╗   ███╗██████╗"
